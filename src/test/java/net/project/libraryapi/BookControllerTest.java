7cce8868aaf610e03279900acff7b5e5f6fbc3f2
Robson Murilo <robsonmurilo.f@gmail.com>
2020-07-26 23:02:25 -0300

    Commit inicial

---
+++ src/test/java/net/project/libraryapi/BookControllerTest.java
@@ -0,0 +1,31 @@
+package net.project.libraryapi;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+
+    @Test
+    @DisplayName("Deve criar um livro com sucesso!")
+    public void createBookTest(){
+
+    }
+
+
+}
